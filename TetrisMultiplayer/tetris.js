95f5b672403bd38da3bfd5354a5e79612510f0bf
Pepe3535 <wlazlo.piotr1@gmail.com>
2019-02-11 15:20:11 +0100

    Another commit

--- TetrisMultiplayer/tetris.js
+++ TetrisMultiplayer/tetris.js
@@ -3,16 +3,18 @@ const context = canvas.getContext('2d');
 
 context.scale(20,20)
 
-context.fillStyle = "#000000";
-context.fillRect(0,0,canvas.width,canvas.height);
+
+function draw(){
+    context.fillStyle = "#000000";
+    context.fillRect(0,0,canvas.width,canvas.height);
+    drawMatrix(player.matrix,player.pos);
+}
 
 const matrix = [
     [0,0,0],
     [1,1,1],
     [0,1,0],
 ];
-
-
 
 /* => explanation
 (anonyomus function)    
@@ -21,15 +23,40 @@ function(arg){          (arg) => {
 }                       }
 
 */
-function drawMatrix(matrix){
+function drawMatrix(matrix,offset){
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if(value !== 0){
                 context.fillStyle = 'red';
-                context.fillRect(x,y,1,1);
+                context.fillRect(x+offset.x,
+                                y+offset.y,
+                                1,1);
             }
         })
     });
 }
 
-drawMatrix(matrix)
+setInterval(function(){
+    draw()
+    player.pos.y++;
+    //requestAnimationFrame(update);
+},500);
+
+const player = {
+    pos: {x:5, y:5},
+    matrix: matrix, //tu może zrobić tablicę obiektów i robić losowanie
+}
+
+document.addEventListener('keydown',event => {
+    if(event.keyCode === 37){
+        player.pos.x--;
+    }
+    else if(event.keyCode === 39){
+        player.pos.x++;
+    }
+    else if(event.keyCode === 40){
+        player.pos.y++;
+    }
+});
+
+update();
